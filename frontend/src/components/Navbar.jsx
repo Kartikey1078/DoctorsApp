75e38a61fa80b967780725387866778e17c630ee
Kartikey1078 <134483566+Kartikey1078@users.noreply.github.com>
2025-11-08 10:59:51 +0530

    Add 'CREATE ACCOUNT' link to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -110,6 +110,7 @@ const Navbar = () => {
                 <NavLink  onClick={()=> setShowMenu(false)} to='/doctors' ><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
                 <NavLink  onClick={()=> setShowMenu(false)} to='/about' ><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
                 <NavLink  onClick={()=> setShowMenu(false)} to='/contact' ><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+                <NavLink  onClick={()=> setShowMenu(false)} to='/login' ><p className='px-4 py-2 rounded inline-block'>CREATE ACCOUNT</p></NavLink>
             </ul>
         </div>
       </div>
