b26e2b850b76e3fb8de547670fe3dcc5ebf919f8
Kartikey1078 <134483566+Kartikey1078@users.noreply.github.com>
2025-11-08 11:07:36 +0530

    Add admin login link to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -112,6 +112,14 @@ const Navbar = () => {
                 <NavLink  onClick={()=> setShowMenu(false)} to='/about' ><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
                 <NavLink  onClick={()=> setShowMenu(false)} to='/contact' ><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
                 <NavLink  onClick={()=> setShowMenu(false)} to='/login' ><p className='px-4 py-2 rounded inline-block'>CREATE ACCOUNT</p></NavLink>
+                 <a
+              href="https://doctorsappadmin.onrender.com/add-doctor"
+              rel="noreferrer"
+              onClick={() => setShowMenu(false)}
+              className="px-4 py-2 rounded inline-block"
+            >
+              ADMIN LOGIN
+            </a>
             </ul>
         </div>
       </div>
